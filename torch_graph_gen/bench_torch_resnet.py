c1d7771450c58d8b0ecd68a7411d4640220227ad
Ben Ghaem <ben.ghaem@utexas.edu>
2019-12-13 16:03:51 -0600

    tweaks for the linter

--- torch_graph_gen/bench_torch_resnet.py
+++ torch_graph_gen/bench_torch_resnet.py
@@ -5,36 +5,43 @@ import onnxruntime as ort
 import datetime
 import sys
 
-model = torch.hub.load('pytorch/vision:v0.4.2', 'resnet18', pretrained=True)
+model = torch.hub.load("pytorch/vision:v0.4.2", "resnet18", pretrained=True)
 
 batch_len = 64
-total_size = 128*12*4
+total_size = 128 * 12 * 4
 
-torch.onnx.export(model, (torch.tensor(rn_loader.get_random(0,batch_len))), "torch_resnet_18.onnx", verbose=True,
-        keep_initializers_as_inputs=True)
+torch.onnx.export(
+    model,
+    (torch.tensor(rn_loader.get_random(0, batch_len))),
+    "torch_resnet_18.onnx",
+    verbose=True,
+    keep_initializers_as_inputs=True,
+)
 
-#sys.exit(1)
+# sys.exit(1)
 
-model.to('cuda')
+model.to("cuda")
 
 st = datetime.datetime.now()
 with torch.no_grad():
-    for batch_id in range(0,total_size,batch_len):
-        batch = torch.tensor(rn_loader.get_random(batch_id, batch_id + batch_len)).to('cuda')
+    for batch_id in range(0, total_size, batch_len):
+        batch = torch.tensor(rn_loader.get_random(batch_id, batch_id + batch_len)).to(
+            "cuda"
+        )
         out = model(batch).cpu().numpy()
-        #print(out)
+        # print(out)
 
 end = datetime.datetime.now()
 
-print("torch:", end-st)
+print("torch:", end - st)
 
 ort_session = ort.InferenceSession("torch_resnet_18.onnx")
 
 st = datetime.datetime.now()
-for batch_id in range(0,total_size,batch_len):
+for batch_id in range(0, total_size, batch_len):
     batch = {"input.1": rn_loader.get_random(batch_id, batch_id + batch_len)}
     out = ort_session.run(None, batch)
 end = datetime.datetime.now()
 
 
-print("ort:", end-st)
+print("ort:", end - st)
